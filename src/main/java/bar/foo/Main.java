83998f9665d4d4e5f59f7728eda15a40398afbf8
Mathias Rühle <math2306@hotmail.com>
2021-03-03 08:39:14 +0100

    Return foo

---
+++ src/main/java/bar/foo/Main.java
@@ -0,0 +1,12 @@
+package bar.foo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(foo());
+    }
+
+    public static String foo() {
+        return "bar";
+    }
+}
